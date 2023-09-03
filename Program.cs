9397c3eea96ec5444f7aa9fc22f5013ae16e0484
Anand <abhayanandkumaranand@hotmail.com>
2023-09-03 23:26:50 +0530

    Write one string to a text file.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
 //Example #2: Write one string to a text file.
 string text = "A class is the most powerful data type in C#. Like a structure, a class defines the data and behaviour of the data type.";
 File.WriteAllText(@"G:\Lesson17\WriteText.txt", text);
-Console.WriteLine("Hello, World!");
